4c2c2c20b7ee34a65ffd5a214d72fbc6d6e46604
lujiang <HR@51ao.com>
2017-08-25 09:30:47 +0800

    add food

--- addons/food/publics/controllers/DefaultController.php
+++ addons/food/publics/controllers/DefaultController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace app\addons\food\publics\controllers;
-
+use Yii;
 use yii\web\Controller;
 use app\addons\food\models\Food_company;
+use app\addons\food\models\Food_employee;
 /**
  * Default controller for the `publics` module
  */
@@ -16,27 +17,58 @@ class DefaultController extends Controller
     //登录入口
     public function actionIndex()
     {
-    	$this->layout = "layout1";
+        $this->layout = "layout1";
         return $this->render('index');
     }
     //商家登录
     public function actionSuper_login()
     {
-    	$this->layout = "layout1";
-    	return $this->render('super_login');
+        $this->layout = "layout1";
+        return $this->render('super_login');
     }
     //商家申请入住
     public function actionCreate_company()
     {
-    	
-    	return $this->render('create_company');
+        
+        return $this->render('create_company');
 
     }
     //员工登录
     public function actionShop_login()
     {
-    	$this->layout = "layout1";
-    	return $this->render('shop_login');
+ 
+         if (Yii::$app->request->isPost) {
+            $data = Yii::$app->request->post();
+            $employee = Food_employee::find()->where(['phone'=>$data['phone']])->one();
+            if (empty($employee)) {
+                $this->dexit(array('error'=>1,'msg'=>'登录名不对'));
+            }
+            if (md5($data['password']) != $employee['password']) {
+                $this->dexit(array('error'=>1,'msg'=>'登录密码不对'));
+            }else{
+                if($employee['status'] != 1){
+                    $this->dexit(array('error'=>1,'msg'=>'禁止登录'));
+                }
+                $session = Yii::$app->session;
+                session_set_cookie_params(3600*24);
+                $session['employee'] = $employee;
+                $this->dexit(array('error'=>0,'msg'=>'登录成功'));
+            }
+            //echo 1;
+            //die;
+        }
+        $this->layout = "layout1";
+        return $this->render('shop_login');
 
+    }
+
+    public function dexit($data = '') 
+    {
+        if (is_array($data)) {
+            echo json_encode($data);
+        } else {
+            echo $data;
+        }
+        exit();
     }
 }
